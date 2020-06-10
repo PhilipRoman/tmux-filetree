eeb0de549547aef1cd0a94584e648e823efcac5b
PhilipRoman <dev.philipjohn@gmail.com>
2020-06-10 13:03:10 +0300

    Improve performance

--- tmux-filetree.lua
+++ tmux-filetree.lua
@@ -1,6 +1,6 @@
 -- returns the PID of current process in active
 -- tmux pane (or 0 if no pane is active)
-local function shellpid()
+local function activepid()
 	local p = io.popen("tmux list-panes -F '#{pane_active} #{pane_pid}'")
 	for line in p:lines() do
 		if line:match '^1' then
@@ -12,43 +12,47 @@ local function shellpid()
 	return 0
 end
 
--- returns the working directory of the process with given pid
-local function procdir(pid)
-	local p = io.popen("readlink -e /proc/"..pid.."/cwd")
-	local result = p:read()
-	p:close()
-	return result
+-- returns [a symlink to] the working directory of the process with given pid
+local function workingdir(pid)
+	return '/proc/'..pid..'/cwd'
 end
 
-local command = os.getenv("TMUX_FILETREE_COMMAND") or "tree -C -L 1 %s"
+local command = os.getenv("TMUX_FILETREE_COMMAND") or "tree -C -L 1 $(readlink -e %s)"
 
 -- returns the desired output for files in given directory
-local function ls(dir)
+local function listfiles(dir)
 	local cmdline = command:gsub("%%s", dir or '.')
 	local p = io.popen(cmdline)
 	local lines = {}
 	for line in p:lines() do
 		lines[#lines + 1] = line
---[[
-		if #lines > ((os.getenv("LINES") or 12) - 2) then
-			lines[#lines + 1] = "..."
-			break
-		end ]]
 	end
 	p:close()
 	return table.concat(lines, "\n")
 end
 
-local oldtext = ""
+-- read the terminal control byte sequence once
+-- (it may vary for each terminal)
+-- write it to stdout to clear terminal
+local TPUT_CLEAR = io.popen'tput reset; tput cup 0 0':read 'a'
 
-while true do
-	local dir = procdir(shellpid())
-	local text = ls(dir)
-	-- only clear terminal when text has changed
-	if text ~= oldtext then
-		oldtext = text
-		os.execute 'tput reset; tput cup 0 0'
-		print(text)
-		os.execute 'sleep 0.2'
+-- main loop
+function run()
+	local oldhash = ""
+
+	while true do
+		local dir = workingdir(activepid())
+		-- this isn't used for output; it is a faster way of checking if any files have changed
+		local filehash = io.popen('echo ' .. dir .. '/**'):read 'a'
+		-- only clear terminal when text has changed
+		if filehash ~= oldhash then
+			oldhash = filehash
+			io.write(TPUT_CLEAR)
+			print(listfiles(dir))
+			os.execute 'sleep 0.2'
+		end
 	end
 end
+
+-- suppress error messages upon ctrl-c
+pcall(run)
