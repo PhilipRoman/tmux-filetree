20bba012bc4a6cd0136c087d601dbd57b46d09fa
PhilipRoman <dev.philipjohn@gmail.com>
2020-07-08 11:55:37 +0300

    Add -c and -d options

--- tmux-filetree.lua
+++ tmux-filetree.lua
@@ -1,3 +1,34 @@
+local delay = 1.0
+local command = os.getenv("TMUX_FILETREE_COMMAND") or "tree -C -L 1 $(readlink -e %s)"
+
+local function err(...)
+	io.stderr:write(table.concat {...}, "\n")
+	os.exit(1)
+end
+
+-- parse command-line arguments
+arg = arg or {...}
+do
+	local i = 1
+	while i <= #arg do
+		if arg[i] == '-c' then
+			i = i + 1
+			command = arg[i] or err("Expected COMMAND after -c")
+		elseif arg[i]:match '^%-d.+$' then
+			delay = tonumber(arg[i]:match '^%-d([0-9.]+)$') or err("Expected '-dNUMBER', got: ", arg[i])
+		elseif arg[i] == '-d' then
+			i = i + 1
+			delay = tonumber(arg[i] and arg[i]:match '^([0-9.]+)$') or err("Expected '-d NUMBER', got: '-d ", arg[i] or '', "'")
+		elseif arg[i] == '-h' then
+			print "Usage: tmux-filetree [-d NUMBER]"
+			print "  -d NUMBER         Delay between updates in seconds"
+			print "  -c COMMAND        Command to execute to view files, %s is the target directory"
+			os.exit(0)
+		end
+		i = i + 1
+	end
+end
+
 -- returns the PID of current process in active
 -- tmux pane (or 0 if no pane is active)
 local function activepid()
@@ -16,8 +47,6 @@ end
 local function workingdir(pid)
 	return '/proc/'..pid..'/cwd'
 end
-
-local command = os.getenv("TMUX_FILETREE_COMMAND") or "tree -C -L 1 $(readlink -e %s)"
 
 -- returns the desired output for files in given directory
 local function listfiles(dir)
@@ -54,6 +83,6 @@ while true do
 	if fileschanged(dir) then
 		io.write(TPUT_CLEAR)
 		print(listfiles(dir))
-		os.execute 'sleep 0.2'
+		os.execute('sleep ' .. delay)
 	end
 end
